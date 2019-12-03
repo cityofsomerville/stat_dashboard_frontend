8c754a34b7dcab4973629d07825a3912cea24b40
Ari Rizzitano <arizzitano@gmail.com>
2019-12-03 17:00:05 -0500

    add header and footer to match existing styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,98 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+const BASE_URL = 'http://go.somervillema.gov/dashboard';
 
-Header.propTypes = {
-  siteTitle: PropTypes.string
-};
+const Header = () => {
+  const [menuOpen, toggleMenuOpen] = useState(false);
 
-Header.defaultProps = {
-  siteTitle: ``
+  return (
+    <header>
+      <div className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="navbar-inner container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            aria-controls="navbarSupportedContent"
+            aria-expanded={menuOpen}
+            aria-label={`${menuOpen ? 'Close' : 'Open'} navigation`}
+            onClick={() => toggleMenuOpen(!menuOpen)}
+          >
+            <span className="navbar-toggler-icon text-light"></span>
+          </button>
+          <div
+            className={cn('navbar-collapse', { collapse: !menuOpen })}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav small">
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href={`${BASE_URL}/index.html`}
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href={`${BASE_URL}/live.html`}
+                >
+                  Live
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href={`${BASE_URL}/work.html`}
+                >
+                  Work
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href={`${BASE_URL}/play.html`}
+                >
+                  Play
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href={`${BASE_URL}/family.html`}
+                >
+                  Raise a Family
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a
+                  className="nav-link py-1 px-3 text-light active"
+                  href={`${BASE_URL}/daily.html`}
+                >
+                  Daily
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link py-1 px-3 text-light"
+                  href="http://www.somervillema.gov"
+                >
+                  City Home
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="bg-success py-3">
+        <h1 className="container text-center text-light font-weight-light">
+          The City of Somerville's Key Systems Indicators
+        </h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
