cf59cae467e081df9eab2fd0b9a85a19feec5517
Ari Rizzitano <arizzitano@gmail.com>
2020-01-16 10:34:49 -0500

    new queries for 311 yearlong averages

--- src/data/cityWork/requests.js
+++ src/data/cityWork/requests.js
@@ -1,6 +1,6 @@
 import parseISO from 'date-fns/parseISO';
 
-import { SOCRATA_DATASETS } from 'data/Constants';
+import { SOCRATA_DATASETS, DATE_PRESETS } from 'data/Constants';
 import { instance, formatURL, constructDateRangeQuery } from 'data/api';
 
 export const getTickets = async ({ startDate, endDate }) => {
@@ -59,6 +59,43 @@ export const getCityWorkExploreData = async key => {
   return await instance.get(formatURL(SOCRATA_DATASETS.Somerville_Services), {
     params: {
       $where: `${dateRange} and (${typeSelection}) and (${statusSelection})`,
+      $limit: 10000
+    }
+  });
+};
+
+// https://data.somervillema.gov/resource/f7b7-bfkg.json?$select=(count(*)/365) as daily_average,codeDesc&$group=codeDesc&$where=(action_date >= '2019-01-16T00:00:00.000' and action_date < '2020-01-16T23:59:59.999')
+export const dailyAveragePerAction = async () => {
+  const dateRange = constructDateRangeQuery({
+    startDate: parseISO(DATE_PRESETS['1 year'].startDate),
+    endDate: parseISO(DATE_PRESETS['1 year'].endDate),
+    dateField: 'action_date'
+  });
+  return await instance.get(
+    formatURL(SOCRATA_DATASETS.Somerville_Services_Activities),
+    {
+      params: {
+        $select: '(count(*)/365) as daily_average,codeDesc',
+        $where: `${dateRange}`,
+        $group: 'codeDesc',
+        $limit: 10000
+      }
+    }
+  );
+};
+
+// https://data.somervillema.gov/resource/4pyi-uqq6.json?$select=type,(count(*)/52) as weekly_average&$group=type
+export const weeklyAveragesPerCategory = async () => {
+  const dateRange = constructDateRangeQuery({
+    startDate: parseISO(DATE_PRESETS['1 year'].startDate),
+    endDate: parseISO(DATE_PRESETS['1 year'].endDate),
+    dateField: 'created_on'
+  });
+  return await instance.get(formatURL(SOCRATA_DATASETS.Somerville_Services), {
+    params: {
+      $select: 'type,(count(*)/52) as weekly_average',
+      $where: `${dateRange}`,
+      $group: 'type',
       $limit: 10000
     }
   });
