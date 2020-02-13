6cf6710ec54e12bbe3c7bc3e93ec8d8c421603bc
Ari Rizzitano <arizzitano@gmail.com>
2020-02-13 14:39:48 -0500

    avoid prettier formatting

--- src/data/CategoryNames.js
+++ src/data/CategoryNames.js
@@ -20,44 +20,43 @@
   quotation marks. Make sure every line has a comma at the end.
 */
 
+// prettier-ignore
 const CategoryNames = {
   // Quality of Life
-  ANIMALS: 'Animals',
-  DRUGS: 'Drugs',
-  DRUNK: 'Drunk',
-  GROUPS: 'Groups',
-  'HYPO-FND': 'Hypo found',
-  'MV VANDL': 'Motor vehicle vandalism',
-  NOISE: 'Noise',
-  'NOISE/FW': 'Noise/FW',
-  'ROAD RAGE': 'Road rage',
-  UNWANTED: 'Unwanted',
+  "ANIMALS": "Animals",
+  "DRUGS": "Drugs",
+  "DRUNK": "Drunk",
+  "GROUPS": "Groups",
+  "HYPO-FND": "Hypo found",
+  "MV VANDL": "Motor vehicle vandalism",
+  "NOISE": "Noise",
+  "NOISE/FW": "Noise/FW",
+  "ROAD RAGE": "Road rage",
+  "UNWANTED": "Unwanted",
 
   // Criminal Incidents
-  'BURGLARY/BREAKING AND ENTERING': 'Burglary/breaking and entering',
-  'MOTOR VEHICLE THEFT': 'Motor vehicle theft',
-  ROBBERY: 'Robbery',
-  'THEFT FROM MOTOR VEHICLE': 'Theft from motor vehicle',
+  "BURGLARY/BREAKING AND ENTERING": "Burglary/breaking and entering",
+  "MOTOR VEHICLE THEFT": "Motor vehicle theft",
+  "ROBBERY": "Robbery",
+  "THEFT FROM MOTOR VEHICLE": "Theft from motor vehicle",
 
   // Motor Vehicle Citations
-  'INSPECTION/STICKER, NO c90 S20': 'No inspection/sticker',
-  'LICENSE NOT IN POSSESSION c90 S11': 'License not in possession',
-  'LICENSE SUSPENDED, OP MV WITH c90 S23':
-    'Operating motor vehicle with license suspended',
-  'MARKED LANES VIOLATION c89 S4A': 'Marked lanes violation',
-  'OP MV WITH c90 S23': 'Operating motor vehicle with c90 S23',
-  'PASSING VIOLATION c89 S2': 'Passing violation',
-  'SAFETY STANDARDS, MV NOT MEETING RMV c90 S7A':
-    'Motor vehicle not meeting RMV safety standards',
-  'STOP/YIELD, FAIL TO c89 S9': 'Failure to stop/yield',
-  'UNSAFE OPERATION OF MV c90 S13': 'Unsafe operation of motor vehicle',
+  "INSPECTION/STICKER, NO c90 S20": "No inspection/sticker",
+  "LICENSE NOT IN POSSESSION c90 S11": "License not in possession",
+  "LICENSE SUSPENDED, OP MV WITH c90 S23": "Operating motor vehicle with license suspended",
+  "MARKED LANES VIOLATION c89 S4A": "Marked lanes violation",
+  "OP MV WITH c90 S23": "Operating motor vehicle with c90 S23",
+  "PASSING VIOLATION c89 S2": "Passing violation",
+  "SAFETY STANDARDS, MV NOT MEETING RMV c90 S7A": "Motor vehicle not meeting RMV safety standards",
+  "STOP/YIELD, FAIL TO c89 S9": "Failure to stop/yield",
+  "UNSAFE OPERATION OF MV c90 S13": "Unsafe operation of motor vehicle",
 
   // Traffic enforcement
-  'MV STOP': 'Motor vehicle stop',
-  TRAFCOMP: 'Traffic comp',
-  TRESPTOW: 'Trespassing/towing',
-  PARKVIOL: 'Parking violation',
-  BOOTLIST: 'Boot list'
+  "MV STOP": "Motor vehicle stop",
+  "TRAFCOMP": "Traffic comp",
+  "TRESPTOW": "Trespassing/towing",
+  "PARKVIOL": "Parking violation",
+  "BOOTLIST": "Boot list",
 };
 
 export default CategoryNames;
